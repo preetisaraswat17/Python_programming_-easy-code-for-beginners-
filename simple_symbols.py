276278548967727a0f0fb47adcb6334c7f6b0dce
preeti saraswat <preetisaraswat17@gmail.com>
2017-10-20 15:14:36 -0700

    Initial commit

--- simple_symbols.py
+++ simple_symbols.py
@@ -1,5 +1,13 @@
+'''Have the function SimpleSymbols(str) take the str parameter being passed and 
+determine if it is an acceptable sequence by either returning the string true or false. 
+The str parameter will be composed of + and = symbols with several letters between them 
+(ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol. 
+So the string to the left would be false. The string will not be empty and will have at least one letter. '''
+
+
 def SimpleSymbols(str): 
 
+    #pad the string first so that it does not give out of bound errors
     new_str='='+str+'='
     for i in range(0,len(new_str)):
        if new_str[i].isalpha():
@@ -9,4 +17,4 @@ def SimpleSymbols(str):
     return True
     
 # keep this function call here  
-print SimpleSymbols('+d+=3=s+')
+print SimpleSymbols(str)
