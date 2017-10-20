d60c7e11dc1ff71f8b37962d61327c959e1cd6e8
preeti saraswat <preetisaraswat17@gmail.com>
2017-10-20 15:06:17 -0700

    Initial Commit

---
+++ string_reverse.py
@@ -0,0 +1,6 @@
+def FirstReverse(str): 
+
+  # str[start:stop:step] where if step is a negative number it'll loop through the string backwards 
+  return str[::-1]
+    
+print FirstReverse('I am a reversed string') 
