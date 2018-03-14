e64e5d013538cdd240df1954de6ffaaf09be4a9d
preeti saraswat <preetisaraswat17@gmail.com>
2018-03-14 12:57:43 -0700

    Update reverse of a number.py

--- reverse of a number.py
+++ reverse of a number.py
@@ -1,10 +1,10 @@
 n=int(input("Enter number: "))
 rev=0
 while(n>0):
-    dig=n%10
+    dig=n%10 #pull out the last digit
     print('dig:',dig)
-    rev=rev*10+dig
+    rev=rev*10+dig  #save the last digit in rev
     print('rev:',rev)
-    n=n//10
+    n=n//10 #remove last digit from original number
     print('n:',n)
 print("Reverse of the number:",rev)
